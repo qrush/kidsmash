02697e968684895ceaea3b605c88a48fd08a021e
Nick Quaranto <nick@quaran.to>
2010-05-10 16:45:56 -0400

    Game starts fullscreen, added instructions, title fades out

--- kidsmash.js
+++ kidsmash.js
@@ -1,6 +1,3 @@
 $(document).ready(function() {
-  $("#fullscreen").click(function() {
-    //alert( $(window).width());
-    //$("canvas").css("width", $(window).width()).css("height", $(window).height());
-  });
+  $("canvas").focus();
 });
